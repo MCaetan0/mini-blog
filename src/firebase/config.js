5c8d66c5dbe23de1efcf66b8d260b45bc552aa41
MCaetan0 <marceloc.lima349@gmail.com>
2024-04-04 20:04:01 -0300

    init

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6z5z5D-fWcwapga47K7MtrqYf0uekRys",
+  authDomain: "miniblog-bed02.firebaseapp.com",
+  projectId: "miniblog-bed02",
+  storageBucket: "miniblog-bed02.appspot.com",
+  messagingSenderId: "237222936092",
+  appId: "1:237222936092:web:b685030621283b9e1da9d9",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+export { db };
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+//CSS
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <h2>oi</h2>
+    </div>
+  );
+};
+
+export default Home;
