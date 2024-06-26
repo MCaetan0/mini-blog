0c748080b116d065005c3bc25f7c8d3f4d7931e7
MCaetan0 <marceloc.lima349@gmail.com>
2024-06-26 19:26:25 -0300

    ajustes

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,7 +1,39 @@
 import styles from "./CreatePost.module.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [title, setTile] = useState();
+  const [image, setImage] = useState();
+  const [body, setBody] = useState();
+  const [tags, setTags] = useState([]);
+  const [formError, serFormError] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <h2>Criar Post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom título..."
+            onChange={(e) => setTile(e.target.value)}
+            value={title}
+          />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePost;
