1069554065a7502a04c66e530e5e14622fe5d18e
MCaetan0 <marceloc.lima349@gmail.com>
2024-06-30 11:01:47 -0300

    fix

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,4 +1,4 @@
-import styles from "Search.module.css";
+import styles from "./Search.module.css";
 
 import { useFetchDocuments } from "../../hooks/useFetchDocument";
 import { useQuery } from "../../hooks/useQuery";
